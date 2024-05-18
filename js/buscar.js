2ac350ed938341254eacd2073e1f9760d78a41be
caique325 <caique-magalhaes1@outlook.com>
2024-05-17 22:13:37 -0300

    atualizando e testando busca atraves de um local host

--- js/buscar.js
+++ js/buscar.js
@@ -6,15 +6,17 @@ botaoBuscar.addEventListener('click',buscarPaciente)
 
 function buscarPaciente(){
    const xhr = new XMLHttpRequest()
-        xhr.open("GET","https://api-pacientes.herokuapp.com/pacientes")
+        xhr.open("GET","Testado com local host")
         
         xhr.addEventListener('load',function(){
         
         if(xhr.status == 200 )
         {  
             const resposta = this.responseText;
-            const paciente = resposta.JSON.parse(parse)
-            paciente.forEach((element) => {
+            const paciente = JSON.parse(resposta)
+            paciente['pacientes'].forEach((element) => {
+
+                element['imc'] = imc(element.peso,element.altura)
                 adicionarPacienteTabela(element);
             });
         }else
